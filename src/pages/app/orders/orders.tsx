a03ebf7ea6c009e8d5bd89d49e0cb63814aac0a8
Fernando Henrique da Silva <109765433+Fernando2-dev@users.noreply.github.com>
2024-01-29 15:17:33 -0300

    criando o componente de paginação para a tabela

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -2,6 +2,7 @@ import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components
 import { Helmet } from "react-helmet-async"
 import { OrdersTableRow } from "./ordesr-table-row"
 import { OrdersTableFilter } from "./orders-table-filter"
+import { Paginacao } from "@/components/paginacao"
 
 export const Orders = () => {
     return (
@@ -9,29 +10,30 @@ export const Orders = () => {
             <Helmet title="pedidos" />
             <div className="flex flex-col gap-4">
                 <h1 className="text-3xl font-bold tracking-tighter"></h1>
-            </div>
-            <div className="space-y-2.5">
-                <OrdersTableFilter />
-                <div className="border rounded-md">
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead className="w-[64px]"></TableHead>
-                                <TableHead className="w-[140px]">Identificador</TableHead>
-                                <TableHead className="w-[180px]">Realizado há</TableHead>
-                                <TableHead className="w-[140px]">Status</TableHead>
-                                <TableHead>Cliente</TableHead>
-                                <TableHead className="w-[140px]">Total de pedidos</TableHead>
-                                <TableHead className="w-[164px]"></TableHead>
-                                <TableHead className="w-[132px]"></TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            <OrdersTableRow />
-                        </TableBody>
-                    </Table>
-                </div>
 
+                <div className="space-y-2.5">
+                    <OrdersTableFilter />
+                    <div className="border rounded-md">
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[64px]"></TableHead>
+                                    <TableHead className="w-[140px]">Identificador</TableHead>
+                                    <TableHead className="w-[180px]">Realizado há</TableHead>
+                                    <TableHead className="w-[140px]">Status</TableHead>
+                                    <TableHead>Cliente</TableHead>
+                                    <TableHead className="w-[140px]">Total de pedidos</TableHead>
+                                    <TableHead className="w-[164px]"></TableHead>
+                                    <TableHead className="w-[132px]"></TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                <OrdersTableRow />
+                            </TableBody>
+                        </Table>
+                    </div>
+                    <Paginacao pageIndex={0} perPage={10} totalCount={105}/>
+                </div>
             </div>
         </>
     )
